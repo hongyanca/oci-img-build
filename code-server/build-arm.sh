83cfb15d5cc112e2a03a7ae6ba3c5221b0e98042
Hong Yan <contact@yanhong.ca>
2024-08-04 04:44:33 -0600

    feat: add aarch64 version

--- code-server/build-arm.sh
+++ code-server/build-arm.sh
@@ -7,8 +7,7 @@ sudo systemctl stop code-server.service
 docker stop code-server
 docker stop cloudflared-tunnel-code-server
 echo 'Removing dev-container image...'
-docker rmi -f awslabca/dev-container-x64
-docker rmi -f lscr.io/linuxserver/code-server
+docker rmi -f awslabca/dev-container-arm
 
 echo 'Removing all unused data...'
 docker builder prune --all --force
@@ -17,15 +16,15 @@ docker system prune --all --volumes --force
 dangling_volumes=$(docker volume ls -qf dangling=true)
 # If there are dangling volumes, remove them
 if [ ! -z "$dangling_volumes" ]; then
-	echo "Removing dangling Docker volumes..."
-	sudo docker volume rm $dangling_volumes
+  echo "Removing dangling Docker volumes..."
+  sudo docker volume rm $dangling_volumes
 else
-	echo "No dangling Docker volumes found."
+  echo "No dangling Docker volumes found."
 fi
 
-# docker build -t awslabca/dev-container:latest -t awslabca/dev-container:`date "+%Y%m%d"`.1 . --push
-docker build -f Dockerfile-x64 -t awslabca/dev-container-x64:latest . --push
-docker build -f Dockerfile-x64 -t awslabca/dev-container-x64:$(date "+%Y%m%d").1 . --push
+# docker build -f Dockerfile-arm -t awslabca/dev-container-arm:latest .
+docker build -f Dockerfile-arm -t awslabca/dev-container-arm:latest . --push
+docker build -f Dockerfile-arm -t awslabca/dev-container-arm:$(date "+%Y%m%d").1 . --push
 
 cd /apps/code-server
 /usr/bin/docker-compose pull
---
+++ code-server/go-installer-arm.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+#https://stackoverflow.com/questions/17480044/how-to-install-the-current-version-of-go-in-ubuntu-precise
+
+## Define version and build
+## Stable versions: https://go.dev/dl/
+VERSION=1.22
+BUILD=5
+
+## Define package name
+PKG_NAME='go'
+
+## Define go official url
+GO_URL='https://golang.org/dl/'
+
+## Define local path
+LOCAL_PATH='/usr/local'
+
+## Define system library path
+LIB_PATH='/usr/lib'
+
+## Define system binary path
+BIN_PATH='/usr/bin'
+
+## Define EV script filename
+EV_FILE='/etc/profile.d/go-bin-path.sh'
+
+## Defile export path command
+EXPORT_CMD='export PATH=$PATH:/usr/local/go/bin'
+
+## Define script content
+SCRIPT='#!/bin/sh\n'${EXPORT_CMD}
+
+## Define user home dir
+USER_HOME_DIR=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
+
+check_command() {
+  [ -x "$(command -v $PKG_NAME)" ] && echo 0 || echo 1
+}
+
+test_package() {
+  echo
+  code=$(check_command)
+  [ $code -eq 0 ] && go version || echo "Error: $PKG_NAME is not installed." >&2
+  exit $code
+}
+
+is_sudoer() {
+  ## Define error code
+  E_NOTROOT=87 # Non-root exit error.
+
+  ## check if is sudoer
+  if ! $(sudo -l &>/dev/null); then
+    echo 'Error: root privileges are needed to run this script'
+    return $E_NOTROOT
+  fi
+  return 0
+}
+
+if is_sudoer; then
+  ## Remove previous package versions
+  status="$(dpkg-query -W --showformat='${db:Status-Status}' ${PKG_NAME} 2>&1)"
+  if [ $? = 0 ] || [ "${status}" = installed ]; then
+    sudo apt remove --purge --auto-remove -y "${PKG_NAME}"
+  fi
+
+  pkg_path="${LOCAL_PATH}/go"
+  pkg_lib_path="${LIB_PATH}/go"
+
+  ## Download the latest version available.
+  # [ "$BUILD" -eq "0" ] && version_build="${VERSION}" || version_build="${VERSION}.${BUILD}"
+  version_build="${VERSION}.${BUILD}"
+  mkdir "${USER_HOME_DIR}/tmp"
+  cd "${USER_HOME_DIR}/tmp"
+  wget "${GO_URL}go${version_build}.linux-arm64.tar.gz"
+
+  ## Extract and install the downloaded version
+  if [ -f "go${version_build}.linux-arm64.tar.gz" ]; then
+    sudo rm -rf "${pkg_path}" && sudo tar -xzvf "go${version_build}.linux-arm64.tar.gz" -C "${LOCAL_PATH}"
+
+    ## Add symbolic link binary files to system binary and library directories
+    if [ ! -h "${pkg_lib_path}" ]; then
+      sudo ln -sv "${pkg_path}" "${pkg_lib_path}"
+    fi
+
+    binaries='go gofmt'
+    for i in $binaries; do
+      if [ ! -h "${BIN_PATH}/${i}" ]; then
+        sudo ln -sv "${pkg_lib_path}/bin/${i}" "${BIN_PATH}/${i}"
+      fi
+    done
+
+    ## Test your new version.
+    test_package
+  else
+    echo "Error: tar file does not exits." >&2
+    exit 1
+  fi
+else
+  exit $?
+fi
